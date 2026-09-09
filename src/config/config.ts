a863f09722046093fbd600e2b07ff633e001e752
hasya7893828468 <hasyaprakash9908207554@gmail.com>
2026-09-09 12:38:58 +0530

    final

--- src/config/config.ts
+++ src/config/config.ts
@@ -9,7 +9,7 @@ interface AppConfig {
 const apiUrlFromConfig = Constants.expoConfig?.extra?.API_URL as string | undefined;
 
 const dev: AppConfig = {
-    apiUrl: apiUrlFromConfig || "http://192.168.0.126:3000/api",
+    apiUrl: apiUrlFromConfig || "https://bluxurymainbackend.onrender.com/api/api",
     googleMapsApiKey: "AIzaSyBxRrmaaB7iOzxJ6a996auq2ypLMm39b5c",
 };
 
